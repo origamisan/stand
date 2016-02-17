56b930958b40a0f12966b84544da1e9da8bae829
Jason Criss <djc88@cornell.edu>
2016-02-16 20:15:14 -0500

    fixed db login

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'host'      => env('DB_HOST', 'mysql.sxevegan.com'),
+            'database'  => env('DB_DATABASE', 'xvxstand'),
+            'username'  => env('DB_USERNAME', 'xvxstand'),
+            'password'  => env('DB_PASSWORD', 'xvxstandup'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
